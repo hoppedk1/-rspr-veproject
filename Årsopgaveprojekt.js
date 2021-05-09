9151da7e4e9da0597bdd947d0482801fd2c95036
alex40119 <73274109+alex40119@users.noreply.github.com>
2021-05-09 16:16:14 +0200

    Update Årsopgaveprojekt.js

--- Årsopgaveprojekt.js
+++ Årsopgaveprojekt.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("gamearea");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
-var dx = 2;
+var dx = 2; //Her bestemmer vi farten på bolden i x og y retningen
 var dy = -2;
 var ballRadius = 10;
 var paddleHeight = 25;
@@ -10,7 +10,7 @@ var paddleWidth = 167.5;
 var paddleX = (canvas.width-paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
-var brickRowCount = 5;
+var brickRowCount = 5; //Her bestemmer vi hvor mange klodser vi ville have ved at sige hvor mange der skal være i en kolonne og hvor mange kolonner vi vil have
 var brickColumnCount = 13;
 var brickWidth = 75;
 var brickHeight = 20;
@@ -27,7 +27,7 @@ for(var c=0; c<brickColumnCount; c++) {
     }
 }
 
-document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keydown", keyDownHandler, false);//Her laver vi controls til spillet ved at definere hvad der skal ske nå man trykker ned på pilvenstre eller pilhøjre
 document.addEventListener("keyup", keyUpHandler, false);
 function keyDownHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
@@ -47,7 +47,7 @@ function keyUpHandler(e) {
     }
 }
 
-function collisionDetection() {
+function collisionDetection() { //Her laver vi collisionDetection på klodserne og en wincondition ved at man smadre alle klodserne
   for(var c=0; c<brickColumnCount; c++) {
     for(var r=0; r<brickRowCount; r++) {
       var b = bricks[c][r];
@@ -58,35 +58,35 @@ function collisionDetection() {
           score++;
           if(score == brickRowCount*brickColumnCount) {
             alert("YOU WIN, CONGRATS!");
-            document.location.reload();
-            clearInterval(interval); // Needed for Chrome to end game
+            document.location.reload(); //Her genindlæser spillet siden så den kan starte forfra
+            clearInterval(interval);
           }
         }
       }
     }
   }
 }
-function drawScore() {
+function drawScore() { //Her laver vi scoren som man kan se øverst i venstre hjørne
     ctx.font = "16px Arial";
     ctx.fillStyle = "#0095DD";
     ctx.fillText("Score: "+score, 8, 20);
 }
 
-function drawball() {
+function drawball() { //Her bygger vi bolden ved at lave et arc der er 360 grader, ved at gøre det laver vi en cirkel og fylder den så med farve
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
 }
-function drawPaddle() {
+function drawPaddle() { //Her bygger vi paddlen i spillet og bruger de variabler vi har i toppen
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
-function drawBricks() {
+function drawBricks() { //Her bygger spillet klodserne baseret på vores variabler brickRowCount og brickColumnCount
     for(var c=0; c<brickColumnCount; c++) {
         for(var r=0; r<brickRowCount; r++) {
             if(bricks[c][r].status == 1) {
@@ -104,7 +104,7 @@ function drawBricks() {
     }
 }
 
-function draw() {
+function draw() { //Her caller spillet alle de tidligere fumktioner vi har lavet og sætter dem ind i spillet en efter en
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBricks();
   drawball()
@@ -112,14 +112,14 @@ function draw() {
   drawScore();
   collisionDetection();
 
-  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) { //Her foræller vi spillet at hvis boldens hibox er inde i enten højre eller venstre side af spilleboksen skal den ændre retning til det modsatte
     dx = -dx;
 }
 
-if(y + dy < ballRadius) {
+if(y + dy < ballRadius) { //Her fortæller vi spillet at hvis boldens hitbox er inde i toppen skal den skifte retning og hvis den rammer bunden er det game over
     dy = -dy;
 } else if(y + dy > canvas.height-ballRadius) {
-    if(x > paddleX && x < paddleX + paddleWidth) {
+    if(x > paddleX && x < paddleX + paddleWidth) { //Her fortæller vi spillet at hvis bolden rammer paddlen skal det ikke være game over men den skal i stedet ændre retning til det modsatte
         dy = -dy
         dx += 0.25// Fart increase i x retningen hver gang bolden rammer paddlen
         dy -= 0.25// Fart increase i y retningen hver gang bolden rammer paddlen
@@ -133,7 +133,7 @@ if(y + dy < ballRadius) {
 
 
 
-    if(rightPressed) {
+    if(rightPressed) { //Her gør vi så paddlen ikke kan køre ud af banen
         paddleX += 12;
         if (paddleX + paddleWidth > canvas.width){
             paddleX = canvas.width - paddleWidth;
@@ -149,4 +149,4 @@ if(y + dy < ballRadius) {
   x += dx;
   y += dy;
 }
-var interval = setInterval(draw, 10);
+var interval = setInterval(draw, 10); //Her sætter vi gamespeed
